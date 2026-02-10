d6485c6cc0b02f8e093676faf7365789453be3fb
kballadar21 <kevincfjh@gmail.com>
2026-02-10 16:34:57 +0100

    bd

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,42 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+import pyodbc
+
+from config import DB_CONFIG
+
+# variables
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app) # Importante: Permite que el Frontend hable con este Backend
+CORS(app)
+
+def get_connection():
+    conn_str = (
+        f"DRIVER={DB_CONFIG['driver']};"
+        f"SERVER={DB_CONFIG['server']};"
+        f"DATABASE={DB_CONFIG['database']};"
+        f"UID={DB_CONFIG['username']};"
+        f"PWD={DB_CONFIG['password']};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=30;"
+    )
+    return pyodbc.connect(conn_str)
 
 @app.route('/api/test', methods=['GET'])
 def test():
     return jsonify({"mensaje": "¡Backend en Flask funcionando correctamente!"})
+
+@app.route('/api/db-test', methods=['GET'])
+def db_test():
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT 1")
+        return jsonify({"db": "Conexión a Azure SQL OK"})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
