c389c047051988f997ec9e04b6d0588c2ce07abf
IBL\victor.camon.ext <victor.camon.ext@inetum.com>
2026-04-07 10:44:24 +0200

    Backend Api Change

--- frontend/src/services/API.js
+++ frontend/src/services/API.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { msalInstance, graphScopes } from '../auth/AuthConfig'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://timelog-backend.agreeablesea-20b4e4bb.spaincentral.azurecontainerapps.io/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
